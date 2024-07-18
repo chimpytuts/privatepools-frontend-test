ff3f4d01a9a0f758a39e147f45f43d350c546fbd
typicalHuman <ruslanzhezh@zohomail.eu>
2024-07-18 16:45:54 +0500

    upd

--- cypress.config.js
+++ cypress.config.js
@@ -3,19 +3,38 @@ const synpressPlugins = require("@synthetixio/synpress/plugins");
 
 module.exports = defineConfig({
   userAgent: "synpress",
-  chromeWebSecurity: false,
+  chromeWebSecurity: true,
+  defaultCommandTimeout:99999999,
+  execTimeout:99999999,
+  taskTimeout:99999999,
+  requestTimeout:999999999,
+  pageLoadTimeout:9999999999,
+  responseTimeout:9999999999,
   e2e: {
     setupNodeEvents(on, config) {
       synpressPlugins(on, config);
       return config;
     },
+    chromeWebSecurity:true,
+    defaultCommandTimeout: 9999999,
+    pageLoadTimeout: 9999999,
+    execTimeout:9999999,
+    retries:100000,
+    taskTimeout: 9999999,
+    responseTimeout: 9999999,
+    requestTimeout: 9999999,
     testIsolation: true,
-    baseUrl: "https://privatepools-shared-version.vercel.app/",
+    baseUrl: "http://localhost:8080/",
     supportFile: "cypress/support/e2e.js",
     env: {
       SECRET_WORDS: process.env.CYPRESS_SECRET_WORDS,
       METAMASK_PASSWORD: process.env.CYPRESS_METAMASK_PASSWORD,
-      NETWORK: "bsc" // Set to BSC Mainnet
-    }
+      NETWORK: "mainnet",
+      NETWORK_NAME: process.env.NETWORK_NAME,
+      T_NETWORK_NAME: process.env.T_NETWORK_NAME,
+      T_CHAIN_ID: process.env.T_CHAIN_ID,
+      T_RPC_URL: process.env.T_RPC_URL,
+      T_SYMBOL: process.env.T_SYMBOL,
+    },
   },
 });
